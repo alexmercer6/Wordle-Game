f17513c25c63f3f00ac9117fe890a4c554d27f2e
alexmercer <alex.mercer6@hotmail.com>
2022-03-19 15:24:54 +1100

    button adds individual letters to first row

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ const wordleContainer = document.getElementById("wordle-container");
 const keyboardRow1 = document.getElementById("keyboard-row-1");
 const keyboardRow2 = document.getElementById("keyboard-row-2");
 const keyboardRow3 = document.getElementById("keyboard-row-3");
+const userInput = document.getElementById("user-input");
+const submitButton = document.getElementById("submit-button")
+
+let index = 0;
+
+
+submitButton.addEventListener("click", function(){
+    addGuessToSquares();
+})
+
+
 
 for(let i = 0; i < 30; i++) {
     let newDiv = addNewElement("div", wordleContainer, "", "wordle-letter")
@@ -19,7 +30,7 @@ for(let i = 0; i < 9; i++) {
     let thirdLineKey = addNewElement("div", keyboardRow3, "", "keys")
 }
 
-
+const wordleLetters = document.querySelectorAll(".wordle-letter")
 
 
 
@@ -30,4 +41,12 @@ function addNewElement(tag, location, text, className) {
     newElement.classList.add(className)
     location.appendChild(newElement);
     return newElement
+}
+
+function addGuessToSquares() {
+    let eachLetter = userInput.value.split("");
+    for(const letter of eachLetter) {
+        wordleLetters[index].textContent = letter;
+        index +=1;
+    }
 }
